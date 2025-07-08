1c2c1c35250b50e50935492f458f87ae6a289db1
Kenechi Nzewi <caseynzewi@gmail.com>
2025-07-08 16:51:28 +0100

    feat(auth): create route auth/refresh_token

--- api/v1/auth/route.py
+++ api/v1/auth/route.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Request
+from fastapi.responses import JSONResponse
 from fastapi.security import OAuth2PasswordRequestForm
 from typing import Annotated
 from sqlalchemy.orm import Session
@@ -6,7 +7,7 @@ from sqlalchemy.orm import Session
 from api.v1.core.database import get_db
 from .schema import Token, UserRegistration, UserResponse
 from .service import create_user, authenticate_user
-from .utils import create_access_token
+from .utils import create_access_token, create_refresh_token, decode_access_token
 from .model import User
 from ..core.logger import logger
 
@@ -34,5 +35,47 @@ def login(
         "email": user.email
     }
     logger.info(f"User {form_data.username} logged in successfully.")
-    return create_access_token(data)
+    access_token = create_access_token(data)
+    refresh_token = create_refresh_token(data)
 
+    response = JSONResponse(content={
+        "access_token": access_token,
+        "token_type": "bearer"
+    })
+
+    response.set_cookie(
+        key="refresh_token",
+        value=refresh_token,
+        httponly=True,
+        samesite="strict",
+        secure=True,
+        max_age=7 * 24 * 60 * 60
+    )
+
+    return response
+
+@auth.post("refresh_token", response_model=Token)
+def refresh_token(request: Request):
+    refresh_token = request.cookies.get("refresh_token")
+    if not refresh_token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Refresh Token not found"
+        )
+    
+    payload = decode_access_token(refresh_token)
+    if not payload:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid refresh token"
+        )
+    
+    data = {
+        "email": payload["email"]
+    }
+    new_access_token = create_access_token(data)
+
+    return JSONResponse(content={
+        "access_token": new_access_token,
+        "token_type": "bearer"
+    })
--- api/v1/auth/utils.py
+++ api/v1/auth/utils.py
@@ -15,17 +15,15 @@ def hash_password(password: str) -> str:
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
-def create_access_token(data: dict) -> str:
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
     to_encode = data.copy()
     created_time = datetime.now(timezone.utc)
-    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = created_time + (expires_delta or timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp": expire, "iat": created_time})
-    token = jwt.encode(
-        to_encode,
-        settings.JWT_SECRET,
-        algorithm=settings.ALGORITHM
-    )
-    return {"access_token": token}
+    return jwt.encode(to_encode,settings.JWT_SECRET,algorithm=settings.ALGORITHM)
+
+def create_refresh_token(data: dict):
+    create_access_token(data, expires_delta=timedelta(days=7))
 
 def decode_access_token(token: str) -> dict:
     try:
